64428ede08561a9ae69757749087307137c8a8fa
Pamela Badermann <badermann.pamela@gmail.com>
2023-03-02 20:22:09 -0300

    criacao classe caixa

---
+++ src/exercicio 3/index.ts
@@ -0,0 +1,9 @@
+class Caixa {
+  entradas: Array<number>;
+  saidas: Array<number>;
+
+  constructor(entradas: Array<number>, saidas: Array<number>) {
+    this.entradas = entradas;
+    this.saidas = saidas;
+  }
+}
