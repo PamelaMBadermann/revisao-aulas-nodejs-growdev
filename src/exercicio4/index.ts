df279f80ae46bccf9b64d171b2d7a2669872709d
Pamela Badermann <badermann.pamela@gmail.com>
2023-03-14 23:09:08 -0300

    composição

--- src/exercicio4/index.ts
+++ src/exercicio4/index.ts
@@ -1,28 +1,29 @@
 class Animal {
-  public nome: string;
-  public idade: number;
-
-  constructor(nome: string, idade: number) {
-    this.nome = nome;
-    this.idade = idade;
-  }
-
   public andar() {
     console.log('andando');
   }
 }
 
-class Cachorro extends Animal {
+class Cachorro {
+  public nome: string;
+  public idade: number;
   public raca: string;
+  public animal: Animal;
 
-  constructor(nome: string, idade: number, raca: string) {
-    super(nome, idade);
+  constructor(nome: string, idade: number, raca: string, animal: Animal) {
+    this.nome = nome;
+    this.idade = idade;
     this.raca = raca;
+    this.animal = animal;
+  }
+
+  public andar() {
+    this.animal.andar();
   }
 }
 
 function run() {
-  const dog = new Cachorro("doguinho", 10, "golden");
+  const dog = new Cachorro("doguinho", 10, "golden", new Animal());
 
   console.log(dog.nome)
   console.log(dog.idade)
