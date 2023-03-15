e7d58c6fd5ccc31826ba2d89a2514faf245d426e
Pamela Badermann <badermann.pamela@gmail.com>
2023-03-15 00:10:55 -0300

    interfaces

--- src/exercicio4/index.ts
+++ src/exercicio4/index.ts
@@ -42,7 +42,7 @@ class Gato extends Animal {
   }
 }
 
-function main() {
+function run() {
   const dog = new Cachorro("doguinho", 10, "golden");
   const cat = new Gato("gatinho", 4, "sem raca");
 
@@ -50,7 +50,7 @@ function main() {
   cat.emitirSom();
 }
 
-main();
+// run();
 
 interface Automovel {
   ligar(): void;
@@ -61,6 +61,20 @@ interface Automovel {
 }
 
 class Carro implements Automovel {
+  public marca: string;
+  public modelo: string;
+  public potencia: number;
+  public cor: string;
+  public anos: number;
+
+  constructor(marca: string, modelo: string, potencia: number, cor: string, anos: number) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.potencia = potencia;
+    this.cor = cor;
+    this.anos = anos;
+  }
+
   public ligar(): void {
     console.log("ligando o carro");
     this.injetarGasolina();
@@ -86,3 +100,44 @@ class Carro implements Automovel {
     console.log("ligando ar condicionado");
   }
 }
+
+class CarroEletrico implements Automovel {
+  public ligar(): void {
+    console.log("ligando o carro eletrico");
+    this.injetarEnergiaNosComponentes();
+  }
+
+  private injetarEnergiaNosComponentes(): void {
+    console.log("injetando energia nos componentes do carro");
+  }
+
+  public desligar(): void {
+    console.log("desligando carro eletrico");
+  }
+
+  public acelerar(): void {
+    console.log("acelerando carro eletrico");
+  }
+
+  public frear(): void {
+    console.log("freando carro eletrico");
+  }
+
+  public ligarArCondicionado(): void {
+    console.log("ligando ar condicionado");
+  }
+}
+
+function ligarAutomovel(automovel: Automovel) {
+  automovel.ligar();
+}
+
+function main() {
+  const civic = new Carro("Honda", "Civic", 2.0, "Branco", 4);
+  const tesla = new CarroEletrico();
+
+  ligarAutomovel(civic);
+  ligarAutomovel(tesla);
+}
+
+main();
